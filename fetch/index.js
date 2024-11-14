8f684d059baadb26618f32ef75e5bc6f98f676f4
jharry444 <jharrison1@qa.com>
2024-11-14 09:27:23 +0000

    async await

--- fetch/index.js
+++ fetch/index.js
@@ -1,22 +1,32 @@
 const fetchTrainers = () => {
     fetch("http://localhost:8080/trainers")
-    .then(res => res.json())
-    .then(data => console.log(data))
-    .catch(err => console.error(err));
+        .then(res => res.json())
+        .then(data => console.log(data))
+        .catch(err => console.error(err));
 }
 
 fetchTrainers();
 
-const addTrainer = () => {
-fetch("http://localhost:8080/trainers", {
-    method: "post",
-    body: JSON.stringify({name: 'PB'}),
-    headers: {
-        "Content-Type": "application/json"
+const addTrainer = async () => {
+    try {
+        const res = await fetch("http://localhost:8080/trainers", {
+            method: "post",
+            body: JSON.stringify({ name: 'PB' }),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+
+        if (res.status != 201) {
+
+        }
+        const res2 = await fetch("http://localhost:8080/trainers")
+        const json = res2.json();
+        console.log(json);
+    } catch (err) {
+        console.error(err);
+        
     }
-}).then(res => {
-    fetchTrainers();
-}).catch(err => console.error(err))
 }
 
 addTrainer();
